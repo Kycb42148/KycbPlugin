ea64034964f19a4df01519df18bac65348cfbe5b
kycb <kycb42148@gmail.com>
2022-07-05 14:21:06 +0300

    Initial commit

---
+++ src/main/java/kycb/plugin/KycbPlugin.java
@@ -0,0 +1,22 @@
+package kycb.plugin;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class KycbPlugin extends Plugin{
+
+    @Override
+    public void init() {
+        Log.info("Kycb plugin loaded.");
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+    }
+}
