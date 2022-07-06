915400758e501499734c584973c702e863add1e5
kycb <kycb42148@gmail.com>
2022-07-07 02:15:57 +0300

    Add information section to welcome screen

--- src/main/java/kycb/plugin/KycbPlugin.java
+++ src/main/java/kycb/plugin/KycbPlugin.java
@@ -5,14 +5,26 @@ import arc.util.*;
 import kycb.plugin.util.Bundle;
 import mindustry.game.EventType.PlayerJoin;
 import mindustry.gen.Call;
+import mindustry.gen.Player;
 import mindustry.mod.*;
+import mindustry.ui.Menus;
+
+import java.util.Locale;
 
 public class KycbPlugin extends Plugin {
 
+    public static int infoMenuId;
+
     @Override
     public void init() {
+        infoMenuId = Menus.registerMenu((player, option) -> {
+            if (option != 0) return;
+            Locale playerLocale = Bundle.findLocale(player);
+            Call.menu(player.con,-1, Bundle.get("game.info.title", playerLocale), Bundle.get("game.info.text", playerLocale), new String[][]{{Bundle.get("menu.close", playerLocale)}});
+        });
         Events.on(PlayerJoin.class, event -> {
-            Call.menu(event.player.con, 0, Bundle.get("game.rules.title", Bundle.findLocale(event.player)), Bundle.get("game.rules.text", Bundle.findLocale(event.player)), new String[][]{{Bundle.get("menu.close", Bundle.findLocale(event.player))}});
+            Locale playerLocale = Bundle.findLocale(event.player);
+            Call.menu(event.player.con, infoMenuId, Bundle.get("game.rules.title", playerLocale), Bundle.get("game.rules.text", playerLocale), new String[][]{{ Bundle.get("menu.info", playerLocale), Bundle.get("menu.close", playerLocale) }});
         });
         Log.info("Kycb plugin loaded.");
     }
@@ -24,6 +36,8 @@ public class KycbPlugin extends Plugin {
 
     @Override
     public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register("skip", Bundle.get("commands.skip.usage"), Bundle.get("commands.skip.description"), (args, player) -> {
 
+        });
     }
 }
