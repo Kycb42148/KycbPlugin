eff582de167b2ea2d60c9db5776e2b7356c4e8dc
Kycb42148 <kycb42148@gmail.com>
2022-07-06 21:04:38 +0300

    Display rules dialog on player join

--- src/main/java/kycb/plugin/KycbPlugin.java
+++ src/main/java/kycb/plugin/KycbPlugin.java
@@ -1,12 +1,19 @@
 package kycb.plugin;
 
+import arc.Events;
 import arc.util.*;
+import kycb.plugin.util.Bundle;
+import mindustry.game.EventType.PlayerJoin;
+import mindustry.gen.Call;
 import mindustry.mod.*;
 
 public class KycbPlugin extends Plugin {
 
     @Override
     public void init() {
+        Events.on(PlayerJoin.class, event -> {
+            Call.menu(event.player.con, 0, Bundle.get("game.rules.title", Bundle.findLocale(event.player)), Bundle.get("game.rules.text", Bundle.findLocale(event.player)), new String[][]{{Bundle.get("menu.close", Bundle.findLocale(event.player))}});
+        });
         Log.info("Kycb plugin loaded.");
     }
 
